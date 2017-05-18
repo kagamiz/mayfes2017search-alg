18c6f3972bf2eb542f8613eaeceab030c1f63d62
Catupper <nekarugo628@gmail.com>
2017-05-18 15:09:49 +0900

    fix bug

--- route_suggestion/catupper_DP/catupper_main.cpp
+++ route_suggestion/catupper_DP/catupper_main.cpp
@@ -13,6 +13,20 @@ using namespace std;
 
 int n;
 int v[20], s[20], t[20], e[20], d[20][20];
+int **dp, **prev;
+
+int **array_malloc(int a, int b){
+  int **x;
+  x = (int**)malloc(sizeof(int*)*a);
+  for(int i = 0;i < a;i++)
+    x[i] = (int*)malloc(sizeof(int)*b);
+  return x;
+}
+
+void array_free(int **x, int a, int b){
+  for(int i = 0;i < a;i++)free(x[i]);
+  free(x);
+}
 
 int calc(int start, int from, int to){//from, toは1-indexed
   int now = start + d[from][to];//nowはtoに到着する時刻
@@ -34,7 +48,8 @@ int main()
   cin >> s[0];	// 開始時刻
   cin >> e[0];	// 終了時刻
   cin >> t[0];
-
+  dp = array_malloc(1<<n, n);
+  prev = array_malloc(1<<n, n);
   for(int i = 1; i <= n; i++){
     cin >> v[i] >> s[i] >> e[i] >> t[i];	// 入力（頂点番号, 到着したい時刻, 費やす時間）
   }
@@ -45,16 +60,17 @@ int main()
     }
   }
 
-  int dp[1<<n][n], prev[1<<n][n];
   for(int i = 0;i < n;i++){
     for(int j = 0;j < (1 << n);j++){
       dp[j][i] = INF;
     }
   }
   dp[0][0] = s[0];
+  int lastok = 0;
   for(int i = 0;i < (1 << n);i++){ // 訪れた頂点(0-index)
     for(int j = 0;j < n;j++){ // 今いる頂点(0-index)
       if(dp[i][j] == INF)continue;
+      lastok = i;
       for(int k = 0;k < n;k++){ // 次の頂点
 	if(i & (1 << k))continue;
 	int from = j + 1;//1-indexedになおす
@@ -68,13 +84,13 @@ int main()
       }
     }
   }
-
   int last = 0;
   for(int j = 0;j < n;j++){
     if(dp[(1<<n)-1][j] < dp[(1<<n)-1][last]){
       last = j;
     }
   }
+  cout << dp[(1<<n)-1][last] << " "<< e[0] << endl;
   if(dp[(1<<n)-1][last] > e[0]){
     cout << -1 << endl;
     return 0;
@@ -93,5 +109,9 @@ int main()
 //  for(int i = 0; i <= n; i++){ //v[0]は不要
     cout << v[ans[i]+1] << endl;
   }
+
+  array_free(dp, 1<<n, n);
+  array_free(prev, 1<<n, n);
+  
   return 0;
 }
